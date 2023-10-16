da86d81f3fe6e0b1c509c8162dfb2169399c85d4
bsbretly <bsbretly@hotmail.com>
2023-10-16 14:25:53 +0100

    Found error: was not saving initial state correctly.

--- Simulation.py
+++ Simulation.py
@@ -16,9 +16,14 @@ class Sim:
                 q_T, q_dot_T = util.configToTask(q, q_dot, self.robot.dynamics.tool_length)
                 f_e = util.computeRampForce(self.ramp_k, self.ramp_mu, self.planner.p1, self.planner.p2, q_T, q_dot_T)
             else: f_e = np.zeros((2,1))
+            # compute path and control inputs
             V, V_dot = self.planner.step(q, q_dot)
             u, F, F_r, q_r, q_r_dot = self.controller.step(q, q_dot, q_r, q_r_dot, V, V_dot, dt)
-            q, q_dot = self.robot.step(q, q_dot, u, f_e, dt)
+
+            # append data
             f_es.append(f_e.copy()), us.append(u.copy()), qs.append(q.copy()), q_dots.append(q_dot.copy()), q_r_dots.append(q_r_dot.copy()), Fs.append(F.copy()), F_rs.append(F_r.copy()), Vs.append(V.copy()), V_dots.append(V_dot.copy())
+
+            # update state
+            q, q_dot = self.robot.step(q, q_dot, u, f_e, dt)
         return ts, us, Fs, F_rs, f_es, qs, q_dots, q_r_dots, Vs, V_dots
     
