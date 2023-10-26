d55a2de6aba6a0187483fd5648ccc61a62ff5ff3
bsbretly <bsbretly@hotmail.com>
2023-10-26 17:17:09 +0100

    First commit.

---
+++ pvfc_core/Dynamics.py
@@ -0,0 +1,66 @@
+import numpy as np
+import pvfc_core.utilities as util
+
+class QuadrotorDynamics:
+    def __init__(self, robot_parameters, g=9.81):
+        self.m_total, self.I = robot_parameters
+        self.g = g
+    def computeDynamics(self, q, q_dot):
+        M = np.array([[self.m_total, 0, 0], 
+                [0, self.m_total, 0], 
+                [0, 0, self.I]])
+        C = np.zeros((3,3))
+        G = np.array([[0], [self.m_total*self.g], [0]])
+        B = np.array([[np.sin(q[-1,0]), 0],
+            [np.cos(q[-1,0]), 0],
+            [0, 1]])
+        return M, C, G, B
+    
+class QuadrotorTranslationalDynamics(QuadrotorDynamics):
+    # Dynamics for the PVFC controller in translational space of the quadrotor
+    def __init__(self, robot_parameters, g=9.81):
+        super().__init__(robot_parameters, g)
+
+    def computeDynamics(self, q, q_dot):
+        M, C, G, B = super().computeDynamics(q, q_dot)
+        M_trans = M[:2,:2]
+        C_trans = C[:2,:2]
+        return M_trans, C_trans
+
+
+class AerialManipulatorDynamics:
+    def __init__(self, robot_parameters, g=9.81):
+        self.m, self.m_t, self.I, self.I_t, self.tool_length = robot_parameters
+        self.g = g
+        self.m_total = self.m + self.m_t
+    def computeDynamics(self, q, q_dot):
+        M = np.array([[self.m_total, 0, 0, -self.tool_length*self.m_t*np.sin(q[3,0])], 
+            [0, self.m_total, 0, -self.tool_length*self.m_t*np.cos(q[3,0])], 
+            [0, 0, self.I, 0], 
+            [-self.tool_length*self.m_t*np.sin(q[3,0]), -self.tool_length*self.m_t*np.cos(q[3,0]), 0, self.I_t + self.tool_length**2*self.m_t]])
+        C = np.array([[0, 0, 0, -self.tool_length*self.m_t*np.cos(q[3,0])*q_dot[3,0]], 
+            [0, 0, 0, self.tool_length*self.m_t*np.sin(q[3,0])*q_dot[3,0]], 
+            [0, 0, 0, 0], 
+            [0, 0, 0, 0]]) 
+        G = np.array([[0], 
+            [self.g*(self.m_total)], 
+            [0], 
+            [-self.g*self.tool_length*self.m_t*np.cos(q[3,0])]])
+        B = np.array([[np.sin(q[2,0]), 0, 0], 
+            [np.cos(q[2,0]), 0, 0], 
+            [0, 1, -1],
+            [0, 0, 1]])
+        return M, C, G, B
+
+
+class AerialManipulatorTaskDynamics(AerialManipulatorDynamics):
+    # Dynamics for the PVFC controller in task space of the aerial manipulator
+    def __init__(self, robot_parameters):
+        super().__init__(robot_parameters)
+    def computeDynamics(self, q, q_dot):
+        M, C, G, B = super().computeDynamics(q, q_dot)
+        _, J, J_dot = util.computeTransforms(q, q_dot, self.tool_length)
+        M_tilde = np.linalg.inv(J@np.linalg.inv(M)@J.T)
+        C_tilde = np.linalg.inv(J@np.linalg.inv(M)@J.T)@((J@np.linalg.inv(M)@C - J_dot)@np.linalg.pinv(J))
+        return M_tilde, C_tilde
+    
---
+++ pvfc_core/utilities.py
@@ -0,0 +1,47 @@
+import numpy as np
+
+def computeTransforms(q, q_dot, tool_length):
+    '''
+    input: 
+        configuration state and AM tool length - q, q_dot, tool_length
+    output: 
+        forward kinematics, jacobians - K, J, J_dot
+    '''
+    # forward kinematics: configuration space to task space
+    K = np.array([[tool_length*np.cos(q[3,0])], 
+        [-tool_length*np.sin(q[3,0])]])
+    # configuration space to task space jacobian
+    J = np.array([[1, 0, 0, -tool_length*np.sin(q[3,0])], 
+        [0, 1, 0, -tool_length*np.cos(q[3,0])]]) 
+    J_dot = np.array([[0, 0, 0, -tool_length*np.cos(q[3,0])*q_dot[3,0]], 
+        [0, 0, 0, tool_length*np.sin(q[3,0])*q_dot[3,0]]])
+    return K, J, J_dot
+
+def configToTask(q, q_dot, tool_length, q_ddot=None):
+    '''
+    input: 
+        state and AM tool length - q, q_dot, tool_length
+    output: 
+        tool-tip position and velocity - q_T, q_T_dot
+    '''
+    K, J, J_dot = computeTransforms(q, q_dot, tool_length)
+    if isinstance(q_ddot, type(None)): return K + q[:2], J@q_dot
+    return K + q[:2], J@q_dot, J_dot@q_dot + J@q_ddot
+    
+def decomposeThrustVector(Lambda):
+    '''
+    input: 
+        thrust vector - Lambda
+    output: 
+        desired thrust along body z-axis of quadrotor, desired pitch - thrust, theta_d
+    '''
+    thrust = np.linalg.norm(Lambda)
+    Lambda_hat = Lambda/thrust
+    theta_d = np.arctan2(Lambda_hat[0], Lambda_hat[1])[0]
+    return thrust, theta_d
+
+def augmentDynamics(M, C, m_r):
+    M_bar = np.pad(M, ((0,1),(0,1)), 'constant')
+    M_bar[-1,-1] = m_r
+    C_bar = np.pad(C, ((0,1),(0,1)), 'constant')
+    return M_bar, C_bar
